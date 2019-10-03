7b8ca0cbad544b0f03f4cd8b72008cea5d40e10e
Dylan Crawshaw <dcrawsh@gmail.com>
2019-10-03 18:38:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,37 @@
 class Artist
+  @@all = []
+  attr_accessor :name
 
-attr_accessor :name 
-@@all = Array.new 
+  def initialize(name)
+    @name = name
+  end
 
-def initialize(name)
-self.name = name 
-@@all << self 
-end 
+  def self.all
+    @@all    
+  end
+  
+  def songs
+    Song.find_by_artist(self)
+  end
 
-def self.all 
-@@all 
-end 
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
 
-def songs
-Song.all.select {|song| song.artist == self}
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
 end
-
-def add_song(song)
-song.artist = self
-end 
-
-def self.find_or_create_by_name(name)
-Artist.all.detect {|names| name == Artist.name} || artist = Artist.new(name)
-end 
-
-
-
-
-end 
